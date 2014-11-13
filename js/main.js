f3697d2774eefd5d645ecb5083009a1caa8b46b8
hazmatmapper <nost@wisc.edu>
2014-11-12 22:48:43 -0600

    added to main js
    added highlight/dehighlight function
    still need to fix json

--- js/main.js
+++ js/main.js
@@ -2,32 +2,34 @@
 window.onload = initialize(); 
 
 //the first function called once the html is loaded 
-
 function initialize(){
   setMap(); 
   
 }; 
 
 function setMap() {
-var valueById = []; 
+var valueById = new Array (50); 
+for (var i = 0; i < 50; i++) valueById[i] = 0;
 
 d3.csv("data/leadTest.csv", function(data) {
   data.forEach(function(d){
-    d.totalQuantityinShipment = +d.totalQuantityinShipment // convert "Length" column to number
+    d.totalQuantityinShipment = +d.totalQuantityinShipment // convert the quantity of waste from string to number
   });
-  console.log(data);
-  var sum = d3.sum(data.totalQuantityinShipment, function(d) { return d.value; });
+  var sum = d3.sum(data, function(d) { return d.totalQuantityinShipment; }); // sums quantity of waste in shipment for entire set
   console.log(sum);
-}
-);
-// load lead values by state id
-//sum lead values by state id
-/*function() {
-console.log(data.receivingStateCode);
-var sum = d3.sum(data.receivingStateCode["1"], function(d) { return d.value; });
-console.log(sum);
-}*/
+  var nested_data = d3.nest() //d3.nest allows us to sum only parts of a column of data, in this case, sum total waste by state, creating a new array called netsted_data to do so. code adapted from: http://bl.ocks.org/phoebebright/raw/3176159/
+  .key(function(d) { return d.receivingStateCode; }) // set state code as key
+  .rollup(function(leaves) { return {"total_waste": d3.sum(leaves, function(d) {return d.totalQuantityinShipment;})} }) // sum by state code
+  .entries(data);
+  console.log(nested_data[1]["key"]);
+  delete nested_data[10]["key"]; //removes "" key for the waste that had no state listed - will need to rework to make flexible to any kind of waste
+  console.log(nested_data[1]["values"]["total_waste"]);
+  for (var i=0; i<nested_data.length-1; i++) {
+       valueById[nested_data[i]["key"]] = nested_data[i]["values"]["total_waste"];
+        
+};
 
+});
 
 var path = d3.geo.path();
 
@@ -51,11 +53,32 @@ d3.json("data/us.json", function(error, us) {
             x = centroid[0],
             y = centroid[1];
         return "translate(" + x + "," + y + ")"
-            + "scale(" + Math.sqrt(valueById[d.id] * 5 || 0) + ")"
+            + "scale(" + .1/Math.sqrt(valueById[d.id] || 0) + ")"
             + "translate(" + -x + "," + -y + ")";
       })
       .style("stroke-width", function(d) {
-        return 1 / Math.sqrt(valueById[d.id] * 5 || 1);
-      });
+        return .1/Math.sqrt(valueById[d.id] || .01)
+      })
+      .on("mouseover", highlight)
+      .on("mouseout", dehighlight)
+      .on("click", viewer);
 });
+
+function highlight(data){
+  console.log(data.id);
+  //json or csv properties
+  d3.selectAll("."+data.id) //select the current province in the DOM
+    .style({"stroke": "#ffff00", "stroke-width": "5px"}); //set the enumeration unit fill to black
+};
+function dehighlight(data){
+  
+  //json or csv properties
+  var subb = d3.selectAll("."+data.id); //designate selector variable for brevity
+  //var fillcolor = subb.select("desc").text(); //access original color from desc
+  subb.style({"stroke": "#000", "stroke-width": "0px"}); //reset enumeration unit to orginal color
+  };
+function viewer(data){
+  //launch info panel
+  alert("hello world");
+};
 };
