cf9c0b785880b4fea3e3ffbf9c4040b06a6758cb
hazmatmapper <nost@wisc.edu>
2014-11-14 17:51:36 -0600

    highlight, delight, and info panel functionality + a little styling

--- js/main.js
+++ js/main.js
@@ -16,14 +16,11 @@ d3.csv("data/leadTest.csv", function(data) {
     d.totalQuantityinShipment = +d.totalQuantityinShipment // convert the quantity of waste from string to number
   });
   var sum = d3.sum(data, function(d) { return d.totalQuantityinShipment; }); // sums quantity of waste in shipment for entire set
-  console.log(sum);
   var nested_data = d3.nest() //d3.nest allows us to sum only parts of a column of data, in this case, sum total waste by state, creating a new array called netsted_data to do so. code adapted from: http://bl.ocks.org/phoebebright/raw/3176159/
   .key(function(d) { return d.receivingStateCode; }) // set state code as key
   .rollup(function(leaves) { return {"total_waste": d3.sum(leaves, function(d) {return d.totalQuantityinShipment;})} }) // sum by state code
   .entries(data);
-  console.log(nested_data[1]["key"]);
   delete nested_data[10]["key"]; //removes "" key for the waste that had no state listed - will need to rework to make flexible to any kind of waste
-  console.log(nested_data[1]["values"]["total_waste"]);
   for (var i=0; i<nested_data.length-1; i++) {
        valueById[nested_data[i]["key"]] = nested_data[i]["values"]["total_waste"];
         
@@ -31,54 +28,98 @@ d3.csv("data/leadTest.csv", function(data) {
 
 });
 
-var path = d3.geo.path();
+var projection = d3.geo.albers();
+var path = d3.geo.path()
+  .projection(projection);
+
 
 var svg = d3.select("body").append("svg")
     .attr("width", 960)
     .attr("height", 500);
 
-d3.json("data/us.json", function(error, us) {
+d3.json("data/can.json", function(error, can) {
   svg.append("path")
-      .datum(topojson.feature(us, us.objects.land))
+      .datum(topojson.feature(can, can.objects.can))
       .attr("class", "land")
       .attr("d", path);
+});
+
+d3.json("data/mex.json", function(error, mex) {
+  svg.append("path")
+      .datum(topojson.feature(mex, mex.objects.mex))
+      .attr("class", "land")
+      .attr("d", path);
+});
+
+
+
+d3.json("data/us.json", function(error, us) {
 
   svg.selectAll(".state")
-      .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
+      .data(topojson.feature(us, us.objects.usa).features)
+    .enter()
+      .append("g")
       .attr("class", "state")
+      .append("path")
+      .attr("class", function(d) { return d.properties.stateID })
       .attr("d", path)
       .attr("transform", function(d) {
         var centroid = path.centroid(d),
             x = centroid[0],
             y = centroid[1];
         return "translate(" + x + "," + y + ")"
-            + "scale(" + .1/Math.sqrt(valueById[d.id] || 0) + ")"
+            + "scale(" + 100/Math.sqrt(valueById[d.properties.ID_1] || 0) + ")" //need to work on the proper scaling, esp. how to make scaling flexible to other toxics
             + "translate(" + -x + "," + -y + ")";
       })
-      .style("stroke-width", function(d) {
-        return .1/Math.sqrt(valueById[d.id] || .01)
-      })
+      .attr("fill", function(d) {return (valueById[d.properties.ID_1] > 0 ? "#ccc" : "#fff")}) // if state is one actually importing, fill it gray. if not, white
+      /*.style("stroke-width", function(d) {
+        return .1/Math.sqrt(valueById[d.properties.ID_1] || .01)
+      })*/
+      
       .on("mouseover", highlight)
       .on("mouseout", dehighlight)
       .on("click", viewer);
+  lakes();
 });
 
+//load great lakes last so that they overlay canada and us
+function lakes(){
+  d3.json("data/lakes.json", function(error, lakes) {
+  svg.append("path")
+      .datum(topojson.feature(lakes, lakes.objects.lakes))
+      .attr("class", "land")
+      .attr("d", path);
+});
+}
+
 function highlight(data){
-  console.log(data.id);
-  //json or csv properties
-  d3.selectAll("."+data.id) //select the current province in the DOM
-    .style({"stroke": "#ffff00", "stroke-width": "5px"}); //set the enumeration unit fill to black
+  d3.selectAll("."+data.properties.stateID) //select the current province in the DOM
+    .style({"stroke": "#ffff00", "stroke-width": "5px"}); //yellow outline
 };
+
 function dehighlight(data){
-  
   //json or csv properties
-  var subb = d3.selectAll("."+data.id); //designate selector variable for brevity
+  var subb = d3.selectAll("."+data.properties.stateID); //designate selector variable for brevity
   //var fillcolor = subb.select("desc").text(); //access original color from desc
-  subb.style({"stroke": "#000", "stroke-width": "0px"}); //reset enumeration unit to orginal color
+  subb.style({"stroke": "#000", "stroke-width": "1px"}); //reset enumeration unit to orginal color
   };
+
 function viewer(data){
-  //launch info panel
-  alert("hello world");
+  //implement clickoff div - this creates a div that will do two things: 1) make the map more opaque, emphasizing the new info panel; 2) provide a clickable space so that when people click away from the info panel back to the map, the info panel closes
+  d3.select("body")
+    .append("div")
+    .attr("class", "clickoff")
+    .style({"background-color": "#d3d3d3", "opacity": ".3"}) //need to adjust size, color, opacity of div
+    .on("click", function(){
+      d3.selectAll(".viewer").remove()
+      d3.selectAll(".clickoff").remove(); //removes itself so that the map can be clicked again
+    });
+
+  //implement the info panel/viewer here
+  d3.select("body")
+    .append("div")
+    .attr("class", "viewer")
+    .text("this is: "+data.properties.NAME_1+", which imports "+valueById[data.properties.ID_1]+" tons of lead");
 };
+
 };
